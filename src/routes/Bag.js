33ccf410332e8c7f48c9c36f512f6441bf4b7096
arpit-ithub <emailarpityadav@gmail.com>
2024-06-07 20:12:04 +0530

    Add initial file

---
+++ src/routes/Bag.js
@@ -0,0 +1,25 @@
+import BagItem from "../component/BagItem"
+import BagSummary from "../component/BagSummary"
+import { useSelector } from "react-redux";
+
+function Bag() {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id);
+    return itemIndex >= 0;
+  });
+
+  return (
+        <main>
+        <div className="bag-page">
+            <div className="bag-items-container">
+            {finalItems.map((item) => (<BagItem item={item} />))}
+            </div>
+            <BagSummary/>            
+        </div>
+        </main>      
+  )
+}
+
+export default Bag
